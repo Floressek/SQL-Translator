cd29b5577234081d8d0caaa0f6fb16da4d05a1f8
22skowron <szymonskowronnn@gmail.com>
2024-11-22 00:22:58 +0100

    Block going back to login page by logged in useres. Add login guard.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
+import { loginGuard } from './guards/login.guard';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent, canActivate: [authGuard] },
-  { path: 'auth/login', component: LoginPageComponent },
-  {path: 'about', component: AboutPageComponent },
+  {
+    path: 'auth/login',
+    component: LoginPageComponent,
+    canActivate: [loginGuard],
+  },
+  { path: 'about', component: AboutPageComponent },
   { path: '**', redirectTo: '' },
 ];
